8ccf5ad00468508ac68a686c66ea275444d6e1e3
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-11 19:29:54 +0100

    Fix Vercel serverless function crash by updating handler.py

--- handler.py
+++ handler.py
@@ -7,9 +7,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
 # Set default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btamluniverse_project.settings')
 
-# Import the WSGI application from your project's WSGI module
-from btamluniverse_project.wsgi import application
-
-# Expose the application as 'app' (and optionally 'handler')
-app = application
-handler = app
+# Import the WSGI application
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()  # Vercel expects this to be named 'app'

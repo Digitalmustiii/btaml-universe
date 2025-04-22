5223ea8a98de25d90803bd04d3f3cc6ae9ef9cab
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-22 23:08:24 +0100

    lol

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Install Python dependencies
-pip install -r requirements.txt
-
-REM Collect static files
-python manage.py collectstatic --noinput
-
-REM Print success message
-echo Build completed successfully!

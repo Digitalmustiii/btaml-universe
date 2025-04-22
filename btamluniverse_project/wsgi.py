cfbbb2fccd8990a3546acf916d9ff2222141fb82
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-22 11:35:13 +0100

    my changes

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -6,3 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btamluniverse_project.settings"
 application = get_wsgi_application()
 
 app = application
+
+if os.environ.get('VERCEL'):
+    os.system("bash vercel_build.sh")

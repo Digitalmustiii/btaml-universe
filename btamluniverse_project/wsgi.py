7c0feb191d9398913ec889f914865b3f31b8619a
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-22 17:45:56 +0100

    lol

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -3,3 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btamluniverse_project.settings")
 application = get_wsgi_application()
+
+# Expose the WSGI application as "app" and "handler" for Vercel
+app = application
+handler = application

24d1873304196ed6bc0ff92cc221c53b0a4333f0
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-22 14:59:32 +0100

    lets go

--- btamluniverse_project/settings.py
+++ btamluniverse_project/settings.py
@@ -23,6 +23,7 @@ DEBUG        = env('DEBUG')
 ALLOWED_HOSTS = [
     'btamluniverse.pythonanywhere.com',
     'btamluniverse.vercel.app',
+
 ]
 
 # ── APPLICATION DEFINITION ────────────────────────────────────────────────────

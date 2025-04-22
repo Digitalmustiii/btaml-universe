cd2b060b0b8aa494416dd87122448e6d4b5e9df1
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-22 17:50:08 +0100

    lol

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -1,9 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Standard WSGI entrypoint for Django with 'app' exposed for Vercel
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btamluniverse_project.settings")
-application = get_wsgi_application()
-
-# Expose the WSGI application as "app" and "handler" for Vercel
-app = application
-handler = application
+app = get_wsgi_application()

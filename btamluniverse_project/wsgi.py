c8c51cc572ade20e3acd70972546fba5db9a4792
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-22 23:30:51 +0100

    pot

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -7,11 +7,9 @@ application = get_wsgi_application()
 
 # Add WhiteNoise for static file serving
 from whitenoise import WhiteNoise
-application = WhiteNoise(application)
 static_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles')
-if not os.path.exists(static_path):
-    os.makedirs(static_path, exist_ok=True)
+application = WhiteNoise(application)
+# Remove the makedirs line that was causing the error
 application.add_files(static_path, prefix='static/')
 
-# For Vercel
-app = application
+app = application  # For Vercel

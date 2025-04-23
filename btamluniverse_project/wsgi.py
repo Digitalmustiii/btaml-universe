d6c20a73b16c9722acb41ffd08fac5e135673ff7
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 16:09:49 +0100

    lol

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -1,14 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btamluniverse_project.settings")
-
-# Standard Django WSGI app
 application = get_wsgi_application()
 
-# Let WhiteNoise serve /static/ from STATIC_ROOT
-application = WhiteNoise(application, root=str(application.settings.STATIC_ROOT), prefix='static/')
-
-# Also expose as `app` for Vercel’s runtime
+# Expose `app` for Vercel’s Python runtime
 app = application

1b6fc242d5e7791b186e1cba8bc6629e86ce8f1f
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 18:08:37 +0100

    lets go baby

--- handler.py
+++
@@ -1,6 +0,0 @@
-# handler.py
-import os, sys
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btamluniverse_project.settings')
-from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()

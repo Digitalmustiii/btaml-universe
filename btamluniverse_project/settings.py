e447b8965ac2fb1d2f3ad2a9283dc32f19afa4ce
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 21:03:17 +0100

    bypass staticfiles

--- btamluniverse_project/settings.py
+++ btamluniverse_project/settings.py
@@ -27,6 +27,7 @@ ALLOWED_HOSTS = [
     'btamluniverse.pythonanywhere.com',
     'btamluniverse.vercel.app',
     'vercel.app',
+    '127.0.0.1',
 ]
 
 
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
  'django.contrib.sessions.middleware.SessionMiddleware',
  'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -103,10 +103,9 @@ USE_TZ        = True
 
 # ── STATIC & MEDIA ─────────────────────────────────────────────────────────────
 # Replace your current STATIC settings with these
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Serve directly from your GitHub via jsDelivr:
+STATIC_URL = "https://cdn.jsdelivr.net/gh/Digitalmustiii/btamluniverse@main/static/"
+
 
 
 MEDIA_URL = '/media/'

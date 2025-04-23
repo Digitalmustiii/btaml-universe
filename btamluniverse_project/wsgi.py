a001f9889017049d189182a97116626d852fb137
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 14:45:31 +0100

    lol

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -3,11 +3,3 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btamluniverse_project.settings")
 application = get_wsgi_application()
-
-# Add WhiteNoise without trying to modify the filesystem
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
-application.add_files('static', prefix='static/')
-
-# For Vercel
-app = application

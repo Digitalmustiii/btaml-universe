060a0d758aeec894c2cb1b6390ea297920524c44
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 14:22:07 +0100

    lol

--- btamluniverse_project/wsgi.py
+++ btamluniverse_project/wsgi.py
@@ -1,15 +1,13 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Standard WSGI entrypoint for Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btamluniverse_project.settings")
 application = get_wsgi_application()
 
-# Add WhiteNoise for static file serving
+# Add WhiteNoise without trying to modify the filesystem
 from whitenoise import WhiteNoise
-static_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles')
 application = WhiteNoise(application)
-# Remove the makedirs line that was causing the error
-application.add_files(static_path, prefix='static/')
+application.add_files('static', prefix='static/')
 
-app = application  # For Vercel
+# For Vercel
+app = application

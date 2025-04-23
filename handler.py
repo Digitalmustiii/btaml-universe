ad713e0b47a6029bfc4820789a6479fc749b3dd3
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 14:50:10 +0100

    lol

--- handler.py
+++ handler.py
@@ -1,12 +1,6 @@
-import os
-import sys
-
-# Ensure the project directory is in the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-
-# Set default Django settings module
+# handler.py
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btamluniverse_project.settings')
-
-# Import the WSGI application
 from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()  # Vercel expects this to be named 'app'
+app = get_wsgi_application()

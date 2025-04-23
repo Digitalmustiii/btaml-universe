5ef377afbcfbdea07e10280e071d9ddec28b32c6
Digitalmustiii <sanusimustapha387@gmail.com>
2025-04-23 14:19:29 +0100

    lol

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building static files..."
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+/opt/vercel/python3/bin/python -m pip install -r requirements.txt
+/opt/vercel/python3/bin/python manage.py collectstatic --noinput
